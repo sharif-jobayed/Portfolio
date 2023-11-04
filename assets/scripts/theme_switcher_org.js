85cb44ccdec8c30a4eaafb7278d3bea6cd0d4e45
SHARIF MD. JOBAYED -UL- ISLAM <49595427+sharif-jobayed@users.noreply.github.com>
2023-11-04 16:51:30 +0600

    Files added via upload

---
+++ assets/scripts/theme_switcher_org.js
@@ -0,0 +1,31 @@
+
+
+/* Theme Switcher */
+
+const themeDots = document.querySelectorAll (".theme_dot");
+const styleSheet = document.querySelector ("#color_scheme");
+const theme = localStorage.getItem ("theme");
+
+for (var d = 0; d < themeDots.length; d++) {
+	themeDots[d].addEventListener ("click", function () {
+
+		// Fetch dot info
+		let mode = this.dataset.mode;
+
+		// Set Theme
+		setTheme (mode);
+		
+		// Stylesheet changing function
+		function setTheme (dot) {
+			if (dot == `blue`) {
+				styleSheet.href = "./assets/styles/blue.css";
+			} else if (dot == `green`) {
+				styleSheet.href = "./assets/styles/green.css";
+			} else if (dot == `purple`) {
+				styleSheet.href = "./assets/styles/purple.css";
+			} else {
+				styleSheet.href = "./assets/styles/default.css";
+			};
+		};
+	});
+};
